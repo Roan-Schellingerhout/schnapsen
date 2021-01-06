d72ee896aa7e77c0a35dd4e8d45481c428e1062f
Roan-Schellingerhout <roan.schellingerhout@student.uva.nl>
2021-01-06 20:13:59 +0100

    phase 1

--- bots/mybot/mybot.py
+++ bots/mybot/mybot.py
@@ -38,8 +38,9 @@ class Bot:
         trump_moves = [card for card in state.hand() if suits[card // 5] == trump_suit]
 
         if state.get_phase() == 1:
+            # If we move second
             if opcard:
-        
+                # find the opponent's suit and all moves we have in that suit
                 op_suit = suits[opcard // 5]
                 suit_moves = [card for card in state.hand() if suits[card // 5] == op_suit]
         
@@ -47,21 +48,38 @@ class Bot:
                 if trump_suit == op_suit:
                     # find all trump suit cards in our hand that are better than the opponent's trump suit card
                     better_trumps = [move for move in trump_moves if move < opcard]
+                    # play our worst better trump-suit card, or throw away a card if we have none
                     return (max(better_trumps), None) if len(better_trumps) else (max([card % 5 for card in state.hand()]), None)
 
                 # if the opponent played a different suit than the trump suit
                 else:
-                    # if we do not have a trump-suit card, play our worst card in the given suit
-                    if not(trump_moves):
-                        better_in_suit = [move for move in suit_moves if move < opcard]
-                        return (max(better_in_suit), None) if len(better_in_suit) else (max([card % 5 for card in state.hand()]), None)
+                    better_in_suit = [move for move in suit_moves if move < opcard]
 
-                    # if we do have a trump-suit card, play our lowest trump-suit card. 
-                    else:
+                    # if we have a better card in the played suit, play the best one we have.
+                    if better_in_suit:
+                        return (min(better_in_suit), None)
+
+                    # if we don't have a better in-suit card, play our lowest trump-suit card. 
+                    elif trump_moves:
                         return (max(trump_moves), None)
+
+                    # if we have neither, throw our worst card away. 
+                    else:
+                        return (max([card % 5 for card in state.hand()]), None)
             
             else:
-                # if marriage or trump exchange, do it
+                # if we can make a special move (marriage, trump exchange), make it
+                special_moves = [i for i in moves if i[1]]
+                if any(special_moves):
+                    return special_moves[0]
 
-        # Return a random choice
-        return random.choice(moves)
+                # otherwise, play the highest non-trump card in your hand, to force the opponent to trump or give up points
+                else:
+                    return (min([card for card in state.hand() if suits[card // 5] != trump_suit], key = lambda x: x // 5), None)
+        
+        # in phase 2, do other shit.     
+        else:
+            # start by playing high trumps, then move on to high non-trumps
+
+            # Return a random choice
+            return random.choice(moves)
