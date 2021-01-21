cac3601d51046b58dccf23e8403fff6c6439b185
Roan-Schellingerhout <roan.schellingerhout@student.uva.nl>
2021-01-21 13:00:09 +0100

    random stuff you can ignore

--- bots/mybot/mybot.py
+++ bots/mybot/mybot.py
@@ -1,8 +1,3 @@
-"""
-RandomBot -- A simple strategy: enumerates all legal moves, and picks one
-uniformly at random.
-"""
-
 # Import the API objects
 from api import State
 import random
@@ -14,19 +9,6 @@ class Bot:
         pass
 
     def get_move(self, state):
-        # type: (State) -> tuple[int, int]
-        """
-        Function that gets called every turn. This is where to implement the strategies.
-        Be sure to make a legal move. Illegal moves, like giving an index of a card you
-        don't own or proposing an illegal mariage, will lose you the game.
-       	TODO: add some more explanation
-        :param State state: An object representing the gamestate. This includes a link to
-            the states of all the cards, the trick and the points.
-        :return: A tuple of integers or a tuple of an integer and None,
-            indicating a move; the first indicates the card played in the trick, the second a
-            potential spouse.
-        """
-
         # All legal moves
         moves = state.moves()
 
@@ -77,11 +59,10 @@ class Bot:
                 else:
                     return (min([card for card in state.hand() if suits[card // 5] != trump_suit], key = lambda x: x // 5), None)
         
+        # I don't know a good strategy for phase 2 hahahaha. 
         else:
             if not opcard:
                 if trump_moves:
                     return (max(trump_moves), None)
-                else:
-                    return (max(moves, key = lambda x: x[0] % 5))
             # Return a random choice
             return random.choice(moves)
