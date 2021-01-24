29eb2eb1ab10101f4c6e6271676710574923a014
sofi124 <sofi1247@gmail.com>
2021-01-24 21:11:29 +0100

    kb bot completed

--- bots/kbbot/kbbot.py
+++ bots/kbbot/kbbot.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python
-"""
-This is a bot that applies propositional logic reasoning to determine its strategy.
-The strategy it uses is determined by what is defined in load.py. Here it is to always
-pick a Jack to play whenever this is a legal move.
-
-It loads general information about the game, as well as the definition of a strategy,
-from load.py.
-"""
-
-from api import State, util
+from api import State, util, Deck
 import random
 from . import load
 from .kb import KB, Boolean, Integer
+
 
 class Bot:
 
@@ -21,51 +12,271 @@ class Bot:
     def get_move(self, state):
 
         moves = state.moves()
-
         random.shuffle(moves)
+        played_card = state.get_opponents_played_card()
+        beats_opponents_card = []
 
-        for move in moves:
+        def is_trump_suit(card):
+            if Deck.get_suit(card) is state.get_trump_suit():
+                return True
+            return False
 
-            if not self.kb_consistent(state, move):
-                # Plays the first move that makes the kb inconsistent. We do not take
-                # into account that there might be other valid moves according to the strategy.
-                # Uncomment the next line if you want to see that something happens.
-                # print "Strategy Applied"
-                return move
+        def is_same_suit(card1, card2):
+            if Deck.get_suit(card1) is Deck.get_suit(card2):
+                return True
+            return False
 
-        # If no move that is entailed by the kb is found, play random move
+        def is_cheap(card):
+            if Deck.get_rank(card) == "J" or Deck.get_rank(card) == "Q" or Deck.get_rank(card) == "K":
+                return True
+            return False
+
+        if played_card is not None:
+            for move in moves:
+                if is_trump_suit(played_card):
+                    if is_trump_suit(move[0]) and move[0] % 5 < played_card % 5:
+                        beats_opponents_card.append(move)
+                else:
+                    if is_same_suit(move[0], played_card) and move[0] % 5 < played_card % 5:
+                        beats_opponents_card.append(move)
+                    elif is_trump_suit(move[0]):
+                        beats_opponents_card.append(move)
+
+        # game is in phase 1
+        if state.get_phase() == 1:
+            #print("P:1")
+            # in lead
+            if state.get_opponents_played_card() is None:
+                #print("In lead")
+                for move in moves:
+                    # trump exchange
+                    if move[0] is None:
+                        #print("Play trump ex")
+                        return move
+                    # marriage
+                    if move[0] is not None and move[1] is not None:
+                        #print("Play marriage")
+                        return move
+                for move in moves:
+                    if not self.ace_consistent(state, move):
+                        #print("Play any Ace")
+                        return move
+                for move in moves:
+                    if not self.ten_consistent(state, move):
+                        #print("Play any Ten")
+                        return move
+                for move in moves:
+                    if not self.king_consistent(state, move) and not is_trump_suit(move[0]):
+                        #print("Play any K")
+                        return move
+                for move in moves:
+                    if not self.queen_consistent(state, move) and not is_trump_suit(move[0]):
+                        #print("Play any Q")
+                        return move
+                for move in moves:
+                    if not self.jack_consistent(state, move) and not is_trump_suit(move[0]):
+                        #print("Play any J")
+                        return move
+                return random.choice(moves)
+            # opponent in lead
+            else:
+                #print("Op in lead")
+                if is_cheap(played_card) and not is_trump_suit(played_card):
+                    #print("PC non trump J || Q || K")
+                    if len(beats_opponents_card) > 0:
+                        #print("Exists a card that can beat it")
+                        for move in beats_opponents_card:
+                            if not not self.ace_consistent(state, move) and not is_trump_suit(move[0]):
+                                #print("Beat with non trump A")
+                                return move
+                        for move in beats_opponents_card:
+                            if not self.ten_consistent(state, move) and not is_trump_suit(move[0]):
+                                #print("Beat with non trump 10")
+                                return move
+                        for move in beats_opponents_card:
+                            if not self.cheap_consistent(state, move) and not is_trump_suit(move[0]):
+                                # #print("Beat with non trump J || Q || K")
+                                return move
+                        else:
+                            #print("Beat with first trump card")
+                            return beats_opponents_card[0]
+                    # if can't beat the card, play cheapest
+                    else:
+                        #print("Can't beat op card")
+                        for move in moves:
+                            if not self.cheap_consistent(state, move):
+                                #print("Play J || Q || K")
+                                return move
+                            elif not self.ten_consistent(state, move):
+                                #print("Play 10")
+                                return move
+                            elif not self.ace_consistent(state, move):
+                                #print("Play A")
+                                return move
+                            else:
+                                #print("Random move")
+                                return random.choice(moves)
+                # try to win the trick any way possible
+                else:  # opponents cars is 10 or A (trump or non trump)
+                    #print("PC 10 || A (trump || non trump)")
+                    if len(beats_opponents_card) > 0:
+                        #print("Exists a card that can beat it")
+                        #print("Beat with the first card")
+                        return beats_opponents_card[0]
+                    else:  # if can't beat the card, play the cheapest
+                        #print("Can't beat op card")
+                        for move in moves:
+                            if not self.cheap_consistent(state, move):
+                                #print("Play J || Q || K")
+                                return move
+                        for move in moves:
+                            if not self.ten_consistent(state, move):
+                                #print("Play 10")
+                                return move
+                        for move in moves:
+                            if not self.ace_consistent(state, move):
+                                #print("Play A")
+                                return move
+                        else:
+                            #print("Random move")
+                            return random.choice(moves)
+
+                #print("Random move")
+                return random.choice(moves)
+        # game is in phase 2
+        else:
+            #print("P:2")
+            # in lead
+            if state.get_opponents_played_card() is None:
+                #print("In lead")
+                for move in moves:
+                    # marriage
+                    if move[0] is not None and move[1] is not None:
+                        #print("Play marriage")
+                        return move
+                for move in moves:
+                    #print("Play first trump card")
+                    return move
+                for move in moves:
+                    if not is_trump_suit(move[0]):
+                        #print("Play first non trump card")
+                        return move
+            # opponent in lead
+            else:
+                #print("Op in lead")
+                for move in moves:
+                    if is_same_suit(move[0], played_card) and move[0] > played_card:
+                        #print("Play first bigger same suit card")
+                        return move
+                for move in moves:
+                    if is_same_suit(move[0], played_card):
+                        #print("Play first smaller same suit card")
+                        return move
+                for move in moves:
+                    if is_trump_suit(move[0]) and not self.cheap_consistent(state, move):
+                        #print("Play first J || Q || K trump card")
+                        return move
+                for move in moves:
+                    if not self.ten_consistent(self, move) and is_trump_suit(move[0]):
+                        #print("Play first 10 trump card")
+                        return move
+                for move in moves:
+                    if not self.ten_consistent(self, move) and is_trump_suit(move[0]):
+                        #print("Play first A trump card")
+                        return move
+                for move in moves:
+                    #print("Play random card, bc can't beat op")
+                    return random.choice(moves)
+        #print("Random move")
         return random.choice(moves)
 
-    # Note: In this example, the state object is not used,
-    # but you might want to do it for your own strategy.
-    def kb_consistent(self, state, move):
-    # type: (State, move) -> bool
-
-        # each time we check for consistency we initialise a new knowledge-base
+    def jack_consistent(self, state, move):
+        # type: (State, move) -> bool
         kb = KB()
 
-        # Add general information about the game
-        load.general_information(kb)
+        load.jack_information(kb)
+        load.jack_knowledge(kb)
 
-        # Add the necessary knowledge about the strategy
-        load.strategy_knowledge(kb)
-
-        # This line stores the index of the card in the deck.
-        # If this doesn't make sense, refer to _deck.py for the card index mapping
         index = move[0]
 
-        # This creates the string which is used to make the strategy_variable.
-        # Note that as far as kb.py is concerned, two objects created with the same
-        # string in the constructor are equivalent, and are seen as the same symbol.
-        # Here we use "pj" to indicate that the card with index "index" should be played with the
-        # PlayJack heuristics that was defined in class. Initialise a different variable if 
-        # you want to apply a different strategy (that you will have to define in load.py)
-        variable_string = "pj" + str(index)
+        variable_string = "p" + str(index)
         strategy_variable = Boolean(variable_string)
 
-        # Add the relevant clause to the loaded knowledge base
         kb.add_clause(~strategy_variable)
 
-        # If the knowledge base is not satisfiable, the strategy variable is
-        # entailed (proof by refutation)
+        return kb.satisfiable()
+
+    def queen_consistent(self, state, move):
+        kb = KB()
+
+        load.queen_information(kb)
+        load.queen_knowledge(kb)
+
+        index = move[0]
+
+        variable_string = "p" + str(index)
+        strategy_variable = Boolean(variable_string)
+
+        kb.add_clause(~strategy_variable)
+
+        return kb.satisfiable()
+
+    def king_consistent(self, state, move):
+        kb = KB()
+
+        load.king_information(kb)
+        load.king_knowledge(kb)
+
+        index = move[0]
+
+        variable_string = "p" + str(index)
+        strategy_variable = Boolean(variable_string)
+
+        kb.add_clause(~strategy_variable)
+
+        return kb.satisfiable()
+
+    def ten_consistent(self, state, move):
+        kb = KB()
+
+        load.ten_information(kb)
+        load.ten_knowledge(kb)
+
+        index = move[0]
+
+        variable_string = "p" + str(index)
+        strategy_variable = Boolean(variable_string)
+
+        kb.add_clause(~strategy_variable)
+
+        return kb.satisfiable()
+
+    def ace_consistent(self, state, move):
+        kb = KB()
+
+        load.ace_information(kb)
+        load.ace_knowledge(kb)
+
+        index = move[0]
+
+        variable_string = "p" + str(index)
+        strategy_variable = Boolean(variable_string)
+
+        kb.add_clause(~strategy_variable)
+
+        return kb.satisfiable()
+
+    def cheap_consistent(self, state, move):
+        kb = KB()
+
+        load.cheap_information(kb)
+        load.cheap_knowledge(kb)
+
+        index = move[0]
+
+        variable_string = "p" + str(index)
+        strategy_variable = Boolean(variable_string)
+
+        kb.add_clause(~strategy_variable)
+
         return kb.satisfiable()
--- bots/kbbot/load.py
+++ bots/kbbot/load.py
@@ -2,66 +2,180 @@ from .kb import KB, Boolean, Integer
 
 # Initialise all variables that you need for you strategies and game knowledge.
 # Add those variables here.. The following list is complete for the Play Jack strategy.
-J0 = Boolean('j0')
-J1 = Boolean('j1')
-J2 = Boolean('j2')
-J3 = Boolean('j3')
 J4 = Boolean('j4')
-J5 = Boolean('j5')
-J6 = Boolean('j6')
-J7 = Boolean('j7')
-J8 = Boolean('j8')
 J9 = Boolean('j9')
-J10 = Boolean('j10')
-J11 = Boolean('j11')
-J12 = Boolean('j12')
-J13 = Boolean('j13')
 J14 = Boolean('j14')
-J15 = Boolean('j15')
-J16 = Boolean('j16')
-J17 = Boolean('j17')
-J18 = Boolean('j18')
 J19 = Boolean('j19')
-PJ0 = Boolean('pj0')
-PJ1 = Boolean('pj1')
-PJ2 = Boolean('pj2')
-PJ3 = Boolean('pj3')
-PJ4 = Boolean('pj4')
-PJ5 = Boolean('pj5')
-PJ6 = Boolean('pj6')
-PJ7 = Boolean('pj7')
-PJ8 = Boolean('pj8')
-PJ9 = Boolean('pj9')
-PJ10 = Boolean('pj10')
-PJ11 = Boolean('pj11')
-PJ12 = Boolean('pj12')
-PJ13 = Boolean('pj13')
-PJ14 = Boolean('pj14')
-PJ15 = Boolean('pj15')
-PJ16 = Boolean('pj16')
-PJ17 = Boolean('pj17')
-PJ18 = Boolean('pj18')
-PJ19 = Boolean('pj19')
 
-def general_information(kb):
-    # GENERAL INFORMATION ABOUT THE CARDS
-    # This adds information which cards are Jacks
+Q3 = Boolean('q3')
+Q8 = Boolean('q8')
+Q13 = Boolean('q13')
+Q18 = Boolean('q18')
+
+K2 = Boolean('k2')
+K7 = Boolean('k7')
+K12 = Boolean('k12')
+K17 = Boolean('k17')
+
+T1 = Boolean('t1')
+T6 = Boolean('t6')
+T11 = Boolean('t11')
+T16 = Boolean('t16')
+
+A0 = Boolean('a0')
+A5 = Boolean('a5')
+A10 = Boolean('a10')
+A15 = Boolean('a15')
+
+P0 = Boolean('p0')
+P1 = Boolean('p1')
+P2 = Boolean('p2')
+P3 = Boolean('p3')
+P4 = Boolean('p4')
+P5 = Boolean('p5')
+P6 = Boolean('p6')
+P7 = Boolean('p7')
+P8 = Boolean('p8')
+P9 = Boolean('p9')
+P10 = Boolean('p10')
+P11 = Boolean('p11')
+P12 = Boolean('p12')
+P13 = Boolean('p13')
+P14 = Boolean('p14')
+P15 = Boolean('p15')
+P16 = Boolean('p16')
+P17 = Boolean('p17')
+P18 = Boolean('p18')
+P19 = Boolean('p19')
+
+def jack_information(kb):
     kb.add_clause(J4)
     kb.add_clause(J9)
     kb.add_clause(J14)
     kb.add_clause(J19)
-    # Add here whatever is needed for your strategy.
 
-def strategy_knowledge(kb):
-    # DEFINITION OF THE STRATEGY
-    # Add clauses (This list is sufficient for this strategy)
-    # PJ is the strategy to play jacks first, so all we need to model is all x PJ(x) <-> J(x),
-    # In other words that the PJ strategy should play a card when it is a jack
-    kb.add_clause(~J4, PJ4)
-    kb.add_clause(~J9, PJ9)
-    kb.add_clause(~J14, PJ14)
-    kb.add_clause(~J19, PJ19)
-    kb.add_clause(~PJ4, J4)
-    kb.add_clause(~PJ9, J9)
-    kb.add_clause(~PJ14, J14)
-    kb.add_clause(~PJ19, J19)
+def jack_knowledge(kb):
+    kb.add_clause(~J4, P4)
+    kb.add_clause(~J9, P9)
+    kb.add_clause(~J14, P14)
+    kb.add_clause(~J19, P19)
+
+    kb.add_clause(~P4, J4)
+    kb.add_clause(~P9, J9)
+    kb.add_clause(~P14, J14)
+    kb.add_clause(~P19, J19)
+
+def queen_information(kb):
+    kb.add_clause(Q3)
+    kb.add_clause(Q8)
+    kb.add_clause(Q13)
+    kb.add_clause(Q18)
+
+def queen_knowledge(kb):
+    kb.add_clause(~Q3, P3)
+    kb.add_clause(~Q8, P8)
+    kb.add_clause(~Q13, P13)
+    kb.add_clause(~Q18, P18)
+
+    kb.add_clause(~P3, Q3)
+    kb.add_clause(~P8, Q8)
+    kb.add_clause(~P13, Q13)
+    kb.add_clause(~P18, Q18)
+
+def king_information(kb):
+    kb.add_clause(K2)
+    kb.add_clause(K7)
+    kb.add_clause(K12)
+    kb.add_clause(K17)
+
+def king_knowledge(kb):
+    kb.add_clause(~K2, P2)
+    kb.add_clause(~K7, P7)
+    kb.add_clause(~K12, P12)
+    kb.add_clause(~K17, P17)
+
+    kb.add_clause(~P2, K2)
+    kb.add_clause(~P9, K7)
+    kb.add_clause(~P12, K12)
+    kb.add_clause(~P17, K17)
+
+def ten_information(kb):
+    kb.add_clause(T1)
+    kb.add_clause(T6)
+    kb.add_clause(T11)
+    kb.add_clause(T16)
+
+def ten_knowledge(kb):
+    kb.add_clause(~T1, P1)
+    kb.add_clause(~T6, P6)
+    kb.add_clause(~T11, P11)
+    kb.add_clause(~T16, P16)
+
+    kb.add_clause(~P1, T1)
+    kb.add_clause(~P6, T6)
+    kb.add_clause(~P11, T11)
+    kb.add_clause(~P16, T16)
+
+def ace_information(kb):
+    kb.add_clause(A0)
+    kb.add_clause(A5)
+    kb.add_clause(A10)
+    kb.add_clause(A15)
+
+def ace_knowledge(kb):
+    kb.add_clause(~A0, P0)
+    kb.add_clause(~A5, P5)
+    kb.add_clause(~A10, P10)
+    kb.add_clause(~A15, P15)
+
+    kb.add_clause(~P0, A0)
+    kb.add_clause(~P5, A5)
+    kb.add_clause(~P10, A10)
+    kb.add_clause(~P15, A15)
+
+def cheap_information(kb):
+    kb.add_clause(J4)
+    kb.add_clause(J9)
+    kb.add_clause(J14)
+    kb.add_clause(J19)
+
+    kb.add_clause(Q3)
+    kb.add_clause(Q8)
+    kb.add_clause(Q13)
+    kb.add_clause(Q18)
+
+    kb.add_clause(K2)
+    kb.add_clause(K7)
+    kb.add_clause(K12)
+    kb.add_clause(K17)
+
+def cheap_knowledge(kb):
+    kb.add_clause(~J4, P4)
+    kb.add_clause(~J9, P9)
+    kb.add_clause(~J14, P14)
+    kb.add_clause(~J19, P19)
+
+    kb.add_clause(~P4, J4)
+    kb.add_clause(~P9, J9)
+    kb.add_clause(~P14, J14)
+    kb.add_clause(~P19, J19)
+
+    kb.add_clause(~Q3, P3)
+    kb.add_clause(~Q8, P8)
+    kb.add_clause(~Q13, P13)
+    kb.add_clause(~Q18, P18)
+
+    kb.add_clause(~P3, Q3)
+    kb.add_clause(~P8, Q8)
+    kb.add_clause(~P13, Q13)
+    kb.add_clause(~P18, Q18)
+
+    kb.add_clause(~K2, P2)
+    kb.add_clause(~K7, P7)
+    kb.add_clause(~K12, P12)
+    kb.add_clause(~K17, P17)
+
+    kb.add_clause(~P2, K2)
+    kb.add_clause(~P9, K7)
+    kb.add_clause(~P12, K12)
+    kb.add_clause(~P17, K17)
